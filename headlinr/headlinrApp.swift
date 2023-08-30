6dd22885e030bf4cd211f3c478cd86293973c0bf
Michael Dadzie <dadziemikke2@gmail.com>
2023-08-30 21:51:49 +0000

    Initial Commit

---
+++ headlinr/headlinrApp.swift
@@ -0,0 +1,17 @@
+//
+//  headlinrApp.swift
+//  headlinr
+//
+//  Created by Michael Dadzie on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct headlinrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
