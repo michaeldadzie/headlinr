77b4dc13790798a14ca5dc9084b79d875b9f8350
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-01 22:16:33 +0000

    added timeago extension

---
+++ headlinr/Extension/TimeAgoExtension.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+extension Date {
+    func timeAgo() -> String {
+        let calendar = Calendar.current
+        let now = Date()
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: self, to: now)
+
+        if let year = components.year, year > 0 {
+            return "\(year) year\(year == 1 ? "" : "s") ago"
+        } else if let month = components.month, month > 0 {
+            return "\(month) month\(month == 1 ? "" : "s") ago"
+        } else if let day = components.day, day > 0 {
+            return "\(day) day\(day == 1 ? "" : "s") ago"
+        } else if let hour = components.hour, hour > 0 {
+            return "\(hour) hour\(hour == 1 ? "" : "s") ago"
+        } else if let minute = components.minute, minute > 0 {
+            return "\(minute) minute\(minute == 1 ? "" : "s") ago"
+        } else {
+            return "Just now"
+        }
+    }
+}
