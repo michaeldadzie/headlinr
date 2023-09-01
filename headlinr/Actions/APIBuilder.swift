817603aeaa332b76fa7f004feb43d3b7ead36cc1
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-01 13:03:28 +0000

    added api builder

---
+++ headlinr/Actions/APIBuilder.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseUrl: URL { get }
+    var path: String { get }
+}
+
+enum NewsAPI {
+    case getArticles
+}
+
+extension NewsAPI: APIBuilder {
+    
+    var baseUrl: URL {
+        switch self {
+        case .getArticles:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String {
+        return "/news"
+    }
+    
+    var urlRequest: URLRequest {
+        return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+    }
+    
+}
