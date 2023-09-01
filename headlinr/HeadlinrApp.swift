6450acf3e2eb1fbc21ec906f223a31a87c93cbcc
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-01 13:18:20 +0000

    minor refactor

--- headlinr/HeadlinrApp.swift
+++ headlinr/HeadlinrApp.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct headlinrApp: App {
+struct HeadlinrApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
