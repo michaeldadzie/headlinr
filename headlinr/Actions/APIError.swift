82420199b4c0729f690a06cab870efdaa87806b4
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-01 13:02:53 +0000

    added api error enum

---
+++ headlinr/Actions/APIError.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+enum APIError: Error {
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from the service"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown:
+            return "The error is unknown"
+        }
+    }
+}
