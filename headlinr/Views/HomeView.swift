4358828b67f6e8dde85d1ceed7713af54f51cbc3
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-01 13:12:25 +0000

    added home view

---
+++ headlinr/Views/HomeView.swift
@@ -0,0 +1,44 @@
+ import SwiftUI
+
+struct HomeView: View {
+    
+    @Environment(\.openURL) var openUrl
+    @StateObject var viewModel = NewsViewModelImpl(service: NewsServiceImpl())
+    
+    var body: some View {
+        
+        Group {
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
+            case .failed(let error):
+                ErrorView(error: error, handler: viewModel.getArticles)
+            case .success(let articles):
+                NavigationView {
+                    List(articles) { item in
+                        ArticleView(article: item)
+                            .onTapGesture {
+                                load(url: item.url)
+                            }
+                    }
+                    .navigationTitle("Headlinr")
+                }
+            }
+        }
+        .onAppear(perform: viewModel.getArticles)
+    }
+    
+    func load(url: String?) {
+        guard let link = url,
+              let url = URL(string: link) else { return }
+        
+        openUrl(url)
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
