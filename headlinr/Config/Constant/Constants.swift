1c84d00ad32c7f9e760277263e5f99d0cc73aeb6
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-05 18:25:38 +0000

    added consts

---
+++ headlinr/Config/Constant/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum Constants {
+    static let twitter = "https://twitter.com/michaeldadziie"
+    static let email = "mailto:foo@bar.com"
+    static let phone = "tel:123456789"
+}
